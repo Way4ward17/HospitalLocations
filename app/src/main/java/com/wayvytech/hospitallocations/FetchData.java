b405b6b1da929cff7129ed701f3e2f3ebd6af6f7
Adefuwa Oluwaponmile Hussein <34873655+Way4ward17@users.noreply.github.com>
2023-09-29 17:59:31 +0100

    Create FetchData.java

---
+++ app/src/main/java/com/wayvytech/hospitallocations/FetchData.java
@@ -0,0 +1,12 @@
+package com.wayvytech.hospitallocations;
+
+import android.os.AsyncTask;
+
+public class FetchData extends AsyncTask<Object, String,String> {
+    String googleNearplacesData
+
+    @Override
+    protected String doInBackground(Object[] objects) {
+        return null;
+    }
+}
