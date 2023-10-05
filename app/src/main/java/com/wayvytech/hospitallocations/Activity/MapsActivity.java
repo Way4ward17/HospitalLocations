a0adc4f52444b4484b619f345d32a9abde0bb606
Adefuwa Oluwaponmile Hussein <34873655+Way4ward17@users.noreply.github.com>
2023-10-05 22:31:50 +0100

    uPDATES

--- app/src/main/java/com/wayvytech/hospitallocations/Activity/MapsActivity.java
+++ app/src/main/java/com/wayvytech/hospitallocations/Activity/MapsActivity.java
@@ -8,6 +8,7 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -27,6 +28,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.wayvytech.hospitallocations.FetchData;
 import com.wayvytech.hospitallocations.R;
 import com.wayvytech.hospitallocations.databinding.ActivityMapsBinding;
 
@@ -51,7 +53,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
     }
+
 
     /**
      * Manipulates the map once available.
@@ -62,12 +66,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
+
+
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
         getCurrentLocation();
+
+
     }
 
 
@@ -90,17 +99,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             @Override
             public void onLocationResult(@NonNull LocationResult locationResult) {
 
-                Toast.makeText(MapsActivity.this, "Location Result is = "+locationResult, Toast.LENGTH_LONG).show();
+                //Toast.makeText(MapsActivity.this, "Location Result is = "+locationResult, Toast.LENGTH_LONG).show();
 
                 if(locationResult == null){
-                    Toast.makeText(MapsActivity.this, "Location Result is NULL ", Toast.LENGTH_LONG).show();
+                    //Toast.makeText(MapsActivity.this, "Location Result is NULL ", Toast.LENGTH_LONG).show();
                     return;
                 }
 
                 for (Location location : locationResult.getLocations()){
 
                     if(location != null){
-                        Toast.makeText(MapsActivity.this, " Current Location is = "+location, Toast.LENGTH_SHORT).show();
+                        //Toast.makeText(MapsActivity.this, " Current Location is = "+location, Toast.LENGTH_SHORT).show();
                     }
 
                 }
@@ -117,9 +126,28 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     log = location.getLongitude();
 
                     LatLng latLng = new LatLng(lat,log);
-                    mMap.addMarker(new MarkerOptions().position(latLng).title("Current Location"));
+                    mMap.addMarker(new MarkerOptions().position(latLng).title("Current Location")).showInfoWindow();
                     mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
                     mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+
+
+
+                    StringBuilder stringBuilder = new StringBuilder
+                            ("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
+                    stringBuilder.append("location=" + lat + "," + log);
+                    stringBuilder.append("&radius=10000");
+                    stringBuilder.append("&type=hospital");
+                    //stringBuilder. append ("§type=atm");
+                    //stringBuilder. append ("§type=bank");
+                    stringBuilder.append("&sensor=true");
+                    stringBuilder.append("&key=" + getResources().getString(R.string.google_map_key));
+
+                    String url = stringBuilder.toString();
+                    Object dataFetch[] = new Object[2];
+                    dataFetch[0] = mMap;
+                    dataFetch[1] = url;
+                    FetchData fetchData = new FetchData();
+                    fetchData.execute(dataFetch);
                 }
             }
         }).addOnFailureListener(new OnFailureListener() {
@@ -148,4 +176,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
 
     }
+
+
 }
--- app/src/main/java/com/wayvytech/hospitallocations/FetchData.java
+++ app/src/main/java/com/wayvytech/hospitallocations/FetchData.java
@@ -2,11 +2,63 @@ package com.wayvytech.hospitallocations;
 
 import android.os.AsyncTask;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class FetchData extends AsyncTask<Object, String,String> {
-    String googleNearplacesData
+    String googleNearplacesData;
+    GoogleMap googleMap;
+    String url;
+
+    @Override
+    protected void onPostExecute(String s) {
+        try{
+            JSONObject jsonObject = new JSONObject(s);
+            JSONArray jsonArray = jsonObject.getJSONArray("results");
+
+            for(int i = 0; i< jsonArray.length(); i++){
+                JSONObject jsonObject1 = jsonArray.getJSONObject(i);
+                JSONObject getLocation = jsonObject1.getJSONObject("geometry").getJSONObject("location");
+
+                String lat = getLocation.getString("lat");
+                String lng = getLocation.getString("lng");
+
+                JSONObject getName = jsonArray.getJSONObject(i);
+                String name = getName.getString("name");
+
+                LatLng latLng =  new LatLng(Double.parseDouble(lat), Double.parseDouble(lng));
+                MarkerOptions markerOptions = new MarkerOptions();
+                markerOptions.title(name);
+                markerOptions.position(latLng);
+                markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+                Marker marker = googleMap.addMarker(markerOptions);
+                marker.showInfoWindow();
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+
+
+            }
+        }catch (Exception e){
+
+        }
+    }
 
     @Override
     protected String doInBackground(Object[] objects) {
-        return null;
+        try{
+            googleMap = (GoogleMap) objects[0];
+            url = (String) objects[1];
+            DownloadUrl downloadUrl = new DownloadUrl();
+            googleNearplacesData = downloadUrl.retrieveUrl(url);
+        }catch (Exception e){
+                e.printStackTrace();
+        }
+        return googleNearplacesData;
     }
 }
